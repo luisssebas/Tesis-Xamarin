8b9333a87b78233bcdab31930ff037995b53fa46
luissebas <luissebasmanzano@hotmail.com>
2020-02-27 20:34:50 -0500

    Primer Commit

---
+++ AppTesisPagoServicios/AppTesisPagoServicios/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AppTesisPagoServicios.Models;
+
+namespace AppTesisPagoServicios.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
